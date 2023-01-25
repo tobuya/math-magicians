4d4028c0069cdc087d059c6acd08cba5ef004861
tobuya <thomasobuya95@gmail.com>
2023-01-25 22:48:46 +0300

    Modify components to make the full website

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
 import Calculator from './components/Calculator';
+import Quote from './components/Quote';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <Calculator />
+    <Router>
+      <div className={styles.container}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quote />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
