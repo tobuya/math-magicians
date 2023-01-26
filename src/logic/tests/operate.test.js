f32385c900e66f3f18eb7994b9e017328fd86948
Anand8317 <43726580+Anand8317@users.noreply.github.com>
2023-01-26 15:01:14 +0530

    Fixed linters

--- src/logic/tests/operate.test.js
+++ src/logic/tests/operate.test.js
@@ -1,28 +1,28 @@
 import operate from '../operate';
 
-describe( 'Operate logic component test', () => {
+describe('Operate logic component test', () => {
   it('Addition:', () => {
-    expect(operate(3, 2, '+')).toEqual("5");
-  })
+    expect(operate(3, 2, '+')).toEqual('5');
+  });
   it('Subtraction:', () => {
-    expect(operate(3, 2, '-')).toEqual("1");
-  })
+    expect(operate(3, 2, '-')).toEqual('1');
+  });
   it('Multiplication:', () => {
-    expect(operate(3, 2, 'x')).toEqual("6");
-  })
+    expect(operate(3, 2, 'x')).toEqual('6');
+  });
   it('Division:', () => {
-    expect(operate(3, 1, '÷')).toEqual("3");
-  })
+    expect(operate(3, 1, '÷')).toEqual('3');
+  });
   it('Division by zero:', () => {
     expect(operate(3, 0, '÷')).toEqual("Can't divide by 0.");
-  })
+  });
   it('Modulus:', () => {
-    expect(operate(3, 1, '%')).toEqual("0");
-  })
+    expect(operate(3, 1, '%')).toEqual('0');
+  });
   it('Unknown operator:', () => {
     expect(() => operate(1, 2, 'y')).toThrow("Unknown operation 'y'");
-  })
+  });
   it('Unknown operand:', () => {
-    expect(() => operate('a', 2, 'x')).toThrow("");
-  })
-})
+    expect(() => operate('a', 2, 'x')).toThrow('');
+  });
+});
